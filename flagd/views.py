80a68cc597cc11b8180afbc7bca5772bcaf65728
“Nadia <2763374I@student.gla.ac.uk>
2026-03-12 11:23:12 +0000

    more updates on the tempaltes and views

--- flagd/views.py
+++ flagd/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
-# Create your views here.
+def index(request):
+    context_dict = {}
+
+    #visitor_cookie_handler(request)
+
+    return render(request, 'flagd/templates/flagd/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Abouuuuut")
+
+#def about(request):
+#    return render(request, 'flagd/about.html')
